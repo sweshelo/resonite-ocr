5f400f41f10c7d0f31327e9a21ccad964980831b
sweshelo <sweshelo@gmail.com>
2023-10-27 20:18:47 +0900

    websocket -> http post / resolution changeable

--- src/index.js
+++ src/index.js
@@ -1,125 +1,101 @@
 const fs = require('fs')
-const WebSocket = require('ws');
 const sharp = require('sharp');
 const crypto = require('crypto')
 const { ComputerVisionClient } = require("@azure/cognitiveservices-computervision");
 const { CognitiveServicesCredentials } = require("@azure/ms-rest-azure-js");
+const express = require('express');
+const bodyParser = require('body-parser');
 
 require('dotenv').config();
+
+const generateImage = async(string, width, height) => {
+  const buffer = new Uint8Array( width * height * 4 ).fill(255);
+
+  for(let index = 0; index < (width * height); index++) {
+    const color = parseInt(string[index], 16) * 16
+    buffer[index * 4 + 0] = color
+    buffer[index * 4 + 1] = color
+    buffer[index * 4 + 2] = color
+    buffer[index * 4 + 3] = 255
+  }
+
+  const image = sharp(buffer,
+    {
+      raw: {
+        width,
+        height,
+        channels: 4,
+      },
+    }
+  );
+
+  const fileId = crypto.randomUUID()
+  await image.toFile(`img/${fileId}.png`)
+
+  return fileId
+}
+
+const ocr = async(client, fileId, options) => {
+  const recognizedText = []
+
+  const imagePath = `img/${fileId}.png`;
+  const fileBuffer = fs.readFileSync(imagePath);
+
+  const result = await new Promise((resolve, reject) => {
+    client
+      .readInStream(fileBuffer, options)
+      .then((result) => {
+        resolve(result);
+      })
+      .catch((err) => {
+        reject(err);
+      });
+  })
+
+  while(true){
+    const _result = await client.getReadResult(result['apim-request-id'])
+    _result.analyzeResult?.readResults.map((results) => {
+      results.lines.forEach((line) => {
+        recognizedText.push(line.text)
+      })
+    })
+    if (_result.status === 'succeeded') break
+  }
+
+  return recognizedText.join('')
+}
+
 
 async function main() {
   const computerVisionKey = process.env.AZURE_COMPUTER_VISION_KEY
   const computerVisionEndPoint = process.env.AZURE_COMPUTER_VISION_ENDPOINT
   const cognitiveServiceCredentials = new CognitiveServicesCredentials(computerVisionKey);
   const client = new ComputerVisionClient(cognitiveServiceCredentials, computerVisionEndPoint);
-  const wss = new WebSocket.Server({ port: 8080 });
 
-  const sessionData = {}
+  // Setup server and uses.
+  const app = express();
+  app.use(bodyParser.urlencoded({
+    extended: true
+  }));
+  app.use(bodyParser.json({limit: '100mb'}));
+  app.listen(8080);
 
-  wss.on('connection', (ws) => {
-    const sessionId = crypto.randomUUID()
-    ws.sessionId = sessionId;
-    sessionData[sessionId] = null;
-    console.log(`connection opened: ${sessionId}`)
+  app.post('/image', async(req, res) => {
+    const imageStr = req.param('image')
+    const width = req.param('width') ?? 512
+    const height = req.param('height') ?? 256
+    const result = await generateImage(imageStr, width, height)
+    res.send(result)
+  })
 
-    ws.on('message', async (message) => {
-      switch(message.toString()){
-        case 'init':
-          sessionData[sessionId] = {
-            index: 0,
-            content: '',
-          }
-          console.log(`[session initialized] ${sessionId}`)
-          break
-        case 'comp':
-          console.log(`[complete requested]`)
-          if (sessionData[sessionId] && sessionData[sessionId].content.length === 512*256){
-            await generateImage(sessionData[sessionId].content)
-            console.log(`[completed]`)
-            const text = await ocr()
-            console.log(text)
-            ws.send(JSON.stringify(text))
-          }else{
-            console.log(`[complete failed] ${sessionData[sessionId].content.length}`)
-          }
-          break
-        default:
-          if (sessionData[sessionId]){
-            sessionData[sessionId].index++;
-            sessionData[sessionId].content += message;
-          }else{
-            console.log(`[push failed]`)
-          }
-          break
-      }
-    });
-
-    ws.on('close', () => {
-      delete sessionData[sessionId];
-    });
-  });
-
-  const generateImage = async(string) => {
-    const buffer = new Uint8Array( 512 * 256 * 4 ).fill(255);
-    for (let y = 0; y < 256; y++) {
-      for (let x = 0; x < 512; x++) {
-        const index = x + y * 512;
-        const color = parseInt(string[index], 16) * 16
-        buffer[index * 4 + 0] = color
-        buffer[index * 4 + 1] = color
-        buffer[index * 4 + 2] = color
-        buffer[index * 4 + 3] = 255
-      }
-    }
-
-    const image = sharp(buffer,
-      {
-        raw: {
-          width: 512,
-          height: 256,
-          channels: 4,
-        },
-      }
-    );
-
-    const fileStream = await image.toFile('output.png')
-    console.log('File saved:', fileStream);
-  }
-
-  const ocr = async() => {
-    const recognizedText = []
-    const options = {
-      language: "ja"
-    };
-
-    const imagePath = 'output.png';
-    const fileBuffer = fs.readFileSync(imagePath);
-
-    const result = await new Promise((resolve, reject) => {
-      client
-        .readInStream(fileBuffer, options)
-        .then((result) => {
-          resolve(result);
-        })
-        .catch((err) => {
-          reject(err);
-        });
+  app.post('/result/:fileId', async(req, res) => {
+    const fileId = req.param('fileId')
+    const language = req.param('lang') ?? 'ja'
+    const result = await ocr(client, fileId, {
+      language
     })
-
-    while(true){
-      const _result = await client.getReadResult(result['apim-request-id'])
-      console.log(_result)
-      _result.analyzeResult?.readResults.map((results) => {
-        results.lines.forEach((line) => {
-          recognizedText.push(line.text)
-        })
-      })
-      console.log(JSON.stringify(_result))
-      if (_result.status === 'succeeded') break
-    }
-
-    return recognizedText
-  }
+    res.send(result)
+  })
 }
 
 main();
